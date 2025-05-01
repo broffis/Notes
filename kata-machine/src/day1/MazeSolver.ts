5afc5d5b3bc1d9a370dc5cdd066a007934506f1d
Brandon Roffis <brandon.roffis@ferguson.com>
2025-05-01 15:10:13 -0400

    third video

--- kata-machine/src/day1/MazeSolver.ts
+++ kata-machine/src/day1/MazeSolver.ts
@@ -1,9 +1,17 @@
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
+
 function walk(
     maze: string[],
     wall: string,
     curr: Point,
     end: Point,
     seen: boolean[][],
+    path: Point[],
 ): boolean {
     // 1. Base Case
     // Off the map
@@ -26,6 +34,7 @@ function walk(
     // Found the end
     if (curr.x === end.x && curr.y === end.y) {
         // We found the end
+        path.push(curr);
         return true;
     }
 
@@ -34,6 +43,26 @@ function walk(
         // We shouldn't go this way
         return false;
     }
+
+    // Pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // Recurse
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        if (
+            walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path)
+        ) {
+            // We found the end
+            return true;
+        }
+    }
+
+    // Post
+    path.pop();
+
+    return false;
 }
 
 export default function solve(
@@ -41,4 +70,15 @@ export default function solve(
     wall: string,
     start: Point,
     end: Point,
-): Point[] {}
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
+}
